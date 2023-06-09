af9eacad5ffe16803e6d04384163c4caf2072182
Hassan Gomaa <hassangomaa974@gmail.com>
2023-06-09 12:13:56 +0300

    upload file v2

--- src/s3/s3.service.ts
+++ src/s3/s3.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import { S3 } from 'aws-sdk';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import * as multer from 'multer';
 import { Multer } from 'multer';
 
 @Injectable()
 export class S3Service {
-  private readonly s3: S3;
+  private readonly s3: S3Client;
   private readonly upload: Multer;
 
   constructor() {
     // Initialize the AWS SDK S3 client with your credentials and configuration
-    this.s3 = new S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    this.s3 = new S3Client({
       region: process.env.AWS_REGION,
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      },
     });
 
     // Configure multer for file upload
@@ -26,13 +28,15 @@ export class S3Service {
   }
 
   async uploadFile(file: Express.Multer.File): Promise<string> {
-    const uploadParams: S3.PutObjectRequest = {
+    const command = new PutObjectCommand({
       Bucket: process.env.AWS_S3_BUCKET_NAME,
       Key: file.originalname,
       Body: file.buffer,
-    };
+    });
 
-    const response = await this.s3.upload(uploadParams).promise();
-    return response.Location;
+    await this.s3.send(command);
+
+    const fileUrl = `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${file.originalname}`;
+    return fileUrl;
   }
 }
